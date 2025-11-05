30ff30cfb4d85f962c6c56c0774cd58f3355ace5
Rayyan0080 <rayyanomarz@gmail.com>
2025-11-05 14:43:12 -0500

    ex1

--- edu/seg2105/client/backend/ChatClient.java
+++ edu/seg2105/client/backend/ChatClient.java
@@ -4,94 +4,56 @@
 
 package edu.seg2105.client.backend;
 
-import ocsf.client.*;
+// (Keep your existing package line if you have one)
 
-import java.io.*;
+import ocsf.client.AbstractClient;
+import edu.seg2105.client.common.ChatIF
 
-import edu.seg2105.client.common.*;
+public class ChatClient extends AbstractClient {
+  private ChatIF clientUI;
 
-/**
- * This class overrides some of the methods defined in the abstract
- * superclass in order to give more functionality to the client.
- *
- * @author Dr Timothy C. Lethbridge
- * @author Dr Robert Lagani&egrave;
- * @author Fran&ccedil;ois B&eacute;langer
- */
-public class ChatClient extends AbstractClient
-{
-  //Instance variables **********************************************
-  
-  /**
-   * The interface type variable.  It allows the implementation of 
-   * the display method in the client.
-   */
-  ChatIF clientUI; 
-
-  
-  //Constructors ****************************************************
-  
-  /**
-   * Constructs an instance of the chat client.
-   *
-   * @param host The server to connect to.
-   * @param port The port number to connect on.
-   * @param clientUI The interface type variable.
-   */
-  
-  public ChatClient(String host, int port, ChatIF clientUI) 
-    throws IOException 
-  {
-    super(host, port); //Call the superclass constructor
+  public ChatClient(String host, int port, ChatIF clientUI) throws Exception {
+    super(host, port);
     this.clientUI = clientUI;
-    openConnection();
+    openConnection(); // connect on startup
   }
 
-  
-  //Instance methods ************************************************
-    
-  /**
-   * This method handles all data that comes in from the server.
-   *
-   * @param msg The message from the server.
-   */
-  public void handleMessageFromServer(Object msg) 
-  {
-    clientUI.display(msg.toString());
-    
-    
-  }
-
-  /**
-   * This method handles all data coming from the UI            
-   *
-   * @param message The message from the UI.    
-   */
-  public void handleMessageFromClientUI(String message)
-  {
-    try
-    {
+  // Send messages typed by user
+  public void handleMessageFromClientUI(String message) {
+    try {
       sendToServer(message);
-    }
-    catch(IOException e)
-    {
-      clientUI.display
-        ("Could not send message to server.  Terminating client.");
+    } catch (Exception e) {
+      clientUI.display("Could not send message to server. Terminating client.");
       quit();
     }
   }
-  
-  /**
-   * This method terminates the client.
-   */
-  public void quit()
-  {
-    try
-    {
+
+  // Messages arriving from server
+  @Override
+  protected void handleMessageFromServer(Object msg) {
+    clientUI.display(msg.toString());
+  }
+
+  // === NEW: react if the connection is closed (e.g., server stops) ===
+  @Override
+  protected void connectionClosed() {
+    System.out.println("Server has shut down. Client will terminate.");
+    quit();
+  }
+
+  // === NEW: react if an exception occurs on the connection ===
+  @Override
+  protected void connectionException(Exception exception) {
+    System.out.println("Server has shut down. Client will terminate.");
+    quit();
+  }
+
+  public void quit() {
+    try {
       closeConnection();
+    } catch (Exception e) {
+      // ignore
     }
-    catch(IOException e) {}
     System.exit(0);
   }
 }
-//End of ChatClient class
--- edu/seg2105/edu/server/backend/EchoServer.java
+++ edu/seg2105/edu/server/backend/EchoServer.java
@@ -4,107 +4,55 @@ package edu.seg2105.edu.server.backend;
 // license found at www.lloseng.com 
 
 
-import ocsf.server.*;
+//(Keep your existing package line if you have one)
 
-/**
- * This class overrides some of the methods in the abstract 
- * superclass in order to give more functionality to the server.
- *
- * @author Dr Timothy C. Lethbridge
- * @author Dr Robert Lagani&egrave;re
- * @author Fran&ccedil;ois B&eacute;langer
- * @author Paul Holden
- */
-public class EchoServer extends AbstractServer 
-{
-  //Class variables *************************************************
-  
-  /**
-   * The default port to listen on.
-   */
-  final public static int DEFAULT_PORT = 5555;
-  
-  //Constructors ****************************************************
-  
-  /**
-   * Constructs an instance of the echo server.
-   *
-   * @param port The port number to connect on.
-   */
-  public EchoServer(int port) 
-  {
-    super(port);
-  }
+import ocsf.server.AbstractServer;
+import ocsf.server.ConnectionToClient;
 
-  
-  //Instance methods ************************************************
-  
-  /**
-   * This method handles any messages received from the client.
-   *
-   * @param msg The message received from the client.
-   * @param client The connection from which the message originated.
-   */
-  public void handleMessageFromClient
-    (Object msg, ConnectionToClient client)
-  {
-    System.out.println("Message received: " + msg + " from " + client);
-    this.sendToAllClients(msg);
-  }
-    
-  /**
-   * This method overrides the one in the superclass.  Called
-   * when the server starts listening for connections.
-   */
-  protected void serverStarted()
-  {
-    System.out.println
-      ("Server listening for connections on port " + getPort());
-  }
-  
-  /**
-   * This method overrides the one in the superclass.  Called
-   * when the server stops listening for connections.
-   */
-  protected void serverStopped()
-  {
-    System.out.println
-      ("Server has stopped listening for connections.");
-  }
-  
-  
-  //Class methods ***************************************************
-  
-  /**
-   * This method is responsible for the creation of 
-   * the server instance (there is no UI in this phase).
-   *
-   * @param args[0] The port number to listen on.  Defaults to 5555 
-   *          if no argument is entered.
-   */
-  public static void main(String[] args) 
-  {
-    int port = 0; //Port to listen on
+public class EchoServer extends AbstractServer {
 
-    try
-    {
-      port = Integer.parseInt(args[0]); //Get port from command line
-    }
-    catch(Throwable t)
-    {
-      port = DEFAULT_PORT; //Set port to 5555
-    }
-	
-    EchoServer sv = new EchoServer(port);
-    
-    try 
-    {
-      sv.listen(); //Start listening for connections
-    } 
-    catch (Exception ex) 
-    {
-      System.out.println("ERROR - Could not listen for clients!");
-    }
-  }
+public EchoServer(int port) {
+ super(port);
 }
-//End of EchoServer class
+
+// Echo messages from clients
+@Override
+protected void handleMessageFromClient(Object msg, ConnectionToClient client) {
+ this.sendToAllClients(msg.toString());
+}
+
+// === NEW: announce when a client connects ===
+@Override
+protected void clientConnected(ConnectionToClient client) {
+ System.out.println("Client connected: " + client);
+}
+
+// === NEW: announce when a client disconnects ===
+@Override
+synchronized protected void clientDisconnected(ConnectionToClient client) {
+ System.out.println("Client disconnected: " + client);
+}
+
+// (Optional, but nice): log server lifecycle
+@Override
+protected void serverStarted() {
+ System.out.println("Server started. Listening on port " + getPort());
+}
+
+@Override
+protected void serverStopped() {
+ System.out.println("Server stopped.");
+}
+
+// Simple main: allow port override via args
+public static void main(String[] args) {
+ int port = 5555;
+ try {
+   if (args.length >= 1) port = Integer.parseInt(args[0]);
+   EchoServer sv = new EchoServer(port);
+   sv.listen(); // start listening
+ } catch (Exception ex) {
+   System.out.println("ERROR - Could not listen for clients!");
+ }
+}
+}
